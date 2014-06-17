918c322a65905e4cf26a9679af6d3652ba70918a
Peter Reid <peter.d.reid@gmail.com>
2014-06-16 21:27:05 -0400

    Start of sensing logic

--- logic-to-audio.asm
+++ logic-to-audio.asm
@@ -4,5 +4,20 @@
  *  Created: 6/16/2014 9:04:35 PM
  *   Author: Peter Reid
  */ 
+ 
+.include "tn85def.inc"
+.ORG 0
+sense_top:
+	// Read our digital inputs
+	IN r16,PORTB
+	// Compare to what we have been reading. If it's the same, we just go around again.
+	CP r16, r17
+	BREQ sense_top
+
+	// Just write the value for now. We will want to store duration too, later.
+	ST X+, r17
 
 
+	MOV r17, r16
+
+RJMP sense_top
